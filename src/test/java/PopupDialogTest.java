70f4722c6673495a2d41272b026a3255951ca127
TatevPetrosyan <tatev.petrosyan.vn@gmail.com>
2015-10-07 00:33:48 +0400

    Notification popup development by David

---
+++ src/test/java/PopupDialogTest.java
@@ -0,0 +1,30 @@
+import arm.davsoft.msggen.utils.Dialogs;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Author: tatevik
+ * Date: 07.10.2015
+ * Time: 00:19
+ */
+public class PopupDialogTest extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Dialogs.showInfoPopup("Title", "Header", "Info");
+        Dialogs.showInfoPopup("Title", null, "Info");
+
+        Dialogs.showWarningPopup("Title", "Header", "Warning");
+        Dialogs.showWarningPopup("Title", null, "Warning");
+
+        Dialogs.showConfirmPopup("Title", "Header", "Confirm");
+        Dialogs.showConfirmPopup("Title", null, "Confirm");
+
+        Dialogs.showExceptionDialog("Header", "Exception", new NullPointerException("Temp exception."));
+        Dialogs.showExceptionDialog(null, "Exception", new NullPointerException("Temp exception."));
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
